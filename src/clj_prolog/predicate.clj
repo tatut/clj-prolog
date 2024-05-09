9b6b4f7f86fe964741a9d48b43601cada2190bb9
Tatu Tarvainen <tatu.tarvainen@me.com>
2024-05-09 12:58:57 +0300

    Support sequence producing fn as backtracking predicate

--- src/clj_prolog/predicate.clj
+++ src/clj_prolog/predicate.clj
@@ -1,5 +1,6 @@
 (ns clj-prolog.predicate
   "Extend Prolog predicates with Clojure."
+  (:refer-clojure :exclude [sequence])
   (:require [clj-prolog.core :refer [*prolog*]]
             [clj-prolog.convert :refer [to-clojure to-prolog]])
   (:import (org.projog.api Projog)
@@ -14,6 +15,21 @@
     SucceedsOncePredicate/SINGLETON
     SucceedsNeverPredicate/SINGLETON))
 
+(defn arity ^long [f]
+  (-> f class .getDeclaredMethods first .getParameterTypes alength))
+
+(defn- add-predicate! [prolog predicate-name retryable? fn-arity get-predicate-fn]
+  (-> prolog .getKnowledgeBase .getPredicates
+      (.addPredicateFactory
+       (PredicateKey. (name predicate-name) fn-arity)
+       (reify PredicateFactory
+         (^Predicate getPredicate [_ ^"[Lorg.projog.core.term.Term;" terms]
+           (if-not (= fn-arity (count terms))
+             (throw (IllegalArgumentException. (str "Expected " fn-arity " arguments.")))
+             (apply get-predicate-fn terms)))
+
+         (^boolean isRetryable [_] (boolean retryable?))))))
+
 (defn lookup
   "Install the given ILookup (like a map) as an arity 2 predicate that looks
   up the first term as key (which must be known) and unifies the found
@@ -22,17 +38,32 @@
   ([^Projog prolog predicate-name lookup]
    (assert (keyword? predicate-name) "Predicate name must be a keyword.")
    (assert (instance? clojure.lang.ILookup lookup))
-   (-> prolog .getKnowledgeBase .getPredicates
-       (.addPredicateFactory
-        (PredicateKey. (name predicate-name) 2)
-        (reify PredicateFactory
-          (^Predicate getPredicate [_ ^"[Lorg.projog.core.term.Term;" terms]
-            (if-not (= 2 (count terms))
-              (throw (IllegalArgumentException. "Lookup needs exactly 2 arguments."))
-              (success1
-               (let [[in out] terms
-                     v (get lookup (to-clojure in) ::not-found)]
-                 (when (not= v ::not-found)
-                   (.unify ^Term out ^Term (to-prolog v)))))))
+   (add-predicate!
+    prolog predicate-name false 2
+    (fn [^Term in ^Term out]
+      (success1
+       (let [v (get lookup (to-clojure in) ::not-found)]
+         (when (not= v ::not-found)
+           (.unify ^Term out ^Term (to-prolog v)))))))))
 
-          (^boolean isRetryable [_] false))))))
+(defn sequence
+  "Install function of arity N as predicate of N+1 where the last
+  term is each successive output of the function (that must return a seqable)."
+  ([predicate-name seq-returning-fn] (sequence *prolog* predicate-name seq-returning-fn))
+  ([prolog predicate-name seq-returning-fn]
+   (add-predicate!
+    prolog predicate-name true (inc (arity seq-returning-fn))
+    (fn [& terms]
+      (let [args (map to-clojure (butlast terms))
+            vals (atom (apply seq-returning-fn args))
+            output (last terms)]
+        (reify Predicate
+          (^boolean evaluate [_this]
+            (boolean
+             (let [vs @vals]
+               (when (seq vs)
+                 (swap! vals rest)
+                 (.backtrack  output)
+                 (.unify output (to-prolog (first vs)))))))
+          (^boolean couldReevaluationSucceed [_this]
+           (boolean (seq @vals)))))))))
--- test/clj_prolog/predicate_test.clj
+++ test/clj_prolog/predicate_test.clj
@@ -1,6 +1,6 @@
 (ns clj-prolog.predicate-test
   (:require [clj-prolog.core :refer [with-prolog q]]
-            [clj-prolog.predicate :refer [lookup]]
+            [clj-prolog.predicate :refer [lookup] :as p]
             [clojure.test :refer [deftest testing is]]))
 
 
@@ -21,3 +21,20 @@
     (testing "Value not found does not succeed"
       (is (empty?
            (q [:paradigm :cobol :Business]))))))
+
+
+(deftest sequence-fn
+  (testing "Simple seq producing fn call"
+    (with-prolog
+      (p/sequence :myrange range)
+      (is (= (list {:N 42} {:N 43})
+             (q [:myrange 42 44 :N])))))
+
+  (testing "Parameters from backtrack between"
+    (with-prolog
+      (p/sequence :myrange range)
+      (is (=
+           (set (for [lo (range 1 4)
+                      n (range lo (* 3 lo))]
+                  {:Lo lo :Hi (* 3 lo) :N n}))
+           (set (q "between(1,3,Lo), Hi is 3 * Lo, myrange(Lo,Hi,N).")))))))
